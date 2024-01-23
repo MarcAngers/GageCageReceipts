0a6e8a7c710ba418d08affe75548af4e64b5b569
Marc Angers <marc.angers99@gmail.com>
2024-01-22 23:40:42 -0500

    woops

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var __DEBUG__ = true;
+    var __DEBUG__ = false;
     var receiptData;
 
     document.getElementById("receiptfile").addEventListener('change', function() {
